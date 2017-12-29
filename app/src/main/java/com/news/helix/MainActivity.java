58bfb4dace84177146a7a5176c2bf9b86de9e950
Jeffrey Lin <jeffless@users.noreply.github.com>
2017-12-28 18:46:17 -0800

    Set up RSS reader

--- app/src/main/java/com/news/helix/MainActivity.java
+++ app/src/main/java/com/news/helix/MainActivity.java
@@ -1,44 +1,100 @@
 package com.news.helix;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import com.bridgefy.sdk.client.Bridgefy;
+import com.bridgefy.sdk.client.BridgefyClient;
+import com.bridgefy.sdk.client.RegistrationListener;
+import com.google.gson.Gson;
+import com.news.helix.adapter.FeedAdapter;
+import com.news.helix.common.HTTPDataHandler;
+import com.news.helix.model.RSSObject;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    RSSObject rssObject;
+
+    private final String RSS_Link = "http://rss.nytimes.com/services/xml/rss/nyt/Science.xml";
+    private final String RSS_To_JSON_API = "https://api.rss2json.com/v1/api.json?rss_url=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create array
-        String[] list = {
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 4",
-                "Item 5",
-                "Item 6"
-        };
-        ListAdapter mainAdapter = new CustomAdapter(this, list); //convert array to list
-        ListView mainListView = (ListView) findViewById(R.id.mainListView);
-        mainListView.setAdapter(mainAdapter);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("News");
+        setSupportActionBar(toolbar);
 
-        mainListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        String list = String.valueOf(adapterView.getItemAtPosition(i));
-                        Toast.makeText(MainActivity.this, list, Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getBaseContext(),
+                LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        loadRSS();
+
+        Bridgefy.initialize(getApplicationContext(), "60771531-18ed-4bc8-bac1-df3908df319c", new RegistrationListener() {
+            @Override
+            public void onRegistrationSuccessful(BridgefyClient bridgefyClient) {
+
+                
+            }
+        });
+    }
+
+    private void loadRSS() {
+        AsyncTask<String, String, String> loadRSSAsync = new AsyncTask<String, String, String>() {
+            ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+
+            @Override
+            protected void onPreExecute() {
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                String result;
+                HTTPDataHandler http = new HTTPDataHandler();
+                result = http.GetHTTPData(strings[0]);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                mDialog.dismiss();
+                rssObject = new Gson().fromJson(s, RSSObject.class);
+                FeedAdapter adapter = new FeedAdapter(rssObject, getBaseContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        StringBuilder url_get_data = new StringBuilder(RSS_To_JSON_API);
+        url_get_data.append(RSS_Link);
+        loadRSSAsync.execute(url_get_data.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_refresh) {
+            loadRSS();
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/news/helix/common/HTTPDataHandler.java
@@ -0,0 +1,44 @@
+package com.news.helix.common;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by green on 12/28/2017.
+ */
+
+public class HTTPDataHandler {
+    static String stream = null;
+
+    public HTTPDataHandler() {
+
+    }
+
+    public String GetHTTPData(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = r.readLine()) != null) {
+                    sb.append(line);
+                }
+                stream = sb.toString();
+                urlConnection.disconnect();
+            }
+        } catch (IOException e) {
+
+        }
+        return stream;
+    }
+}
